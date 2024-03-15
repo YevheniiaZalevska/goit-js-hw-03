383774f7999d56433d142aa7c2b3a49a85c2d9af
YevheniiaZalevska <yevhenia05@gmail.com>
2024-03-15 13:51:13 +0100

    5

--- js/task-2.js
+++ js/task-2.js
@@ -1,25 +1,4 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
 
-
-function formatMessage(message, maxLength) {
-
-    if (message.length <= maxLength) {
-        return message;
-    } else {
-        return message.substring(0, maxLength) + "...";
-    }
-}
-
-// console.log(formatMessage("Curabitur ligula sapien", 16));
-// console.log(formatMessage("Curabitur ligula sapien", 23));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
 
 
 function makeArray(firstArray, secondArray, maxLength) {
