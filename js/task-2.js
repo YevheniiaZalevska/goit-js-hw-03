f27020ad45bcc44dcd58a3f23b6f2460c3e47fd3
YevheniiaZalevska <yevhenia05@gmail.com>
2024-03-15 13:13:34 +0100

    345

--- js/task-2.js
+++ js/task-2.js
@@ -14,9 +14,27 @@ function formatMessage(message, maxLength) {
     }
 }
 
-// console.log(formatMessage("Curabitur ligula sapien", 16)); 
-// console.log(formatMessage("Curabitur ligula sapien", 23)); 
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
+// console.log(formatMessage("Curabitur ligula sapien", 16));
+// console.log(formatMessage("Curabitur ligula sapien", 23));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const combinedArray = firstArray.concat(secondArray);
+
+    if (combinedArray.length > maxLength) {
+        return combinedArray.slice(0, maxLength);
+    }
+
+    return combinedArray;
+}
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); 
--- js/task-3.js
+++ js/task-3.js
@@ -16,8 +16,26 @@ function checkForSpam(message) {
 }
 
 
-// console.log(checkForSpam('Latest technology news')); 
-// console.log(checkForSpam('JavaScript weekly newsletter')); 
-// console.log(checkForSpam('Get best sale offers now!')); 
-// console.log(checkForSpam('Amazing SalE, only tonight!')); 
-// console.log(checkForSpam('SPAM How to earn fast money?')); 
+// console.log(checkForSpam('Latest technology news'));
+// console.log(checkForSpam('JavaScript weekly newsletter'));
+// console.log(checkForSpam('Get best sale offers now!'));
+// console.log(checkForSpam('Amazing SalE, only tonight!'));
+// console.log(checkForSpam('SPAM How to earn fast money?'));
+
+
+function filterArray(numbers, value) {
+    const filteredNumbers = []; 
+
+    for (const number of numbers) { 
+        if (number > value) { 
+            filteredNumbers.push(number); 
+        }
+    }
+  return filteredNumbers; 
+}
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
